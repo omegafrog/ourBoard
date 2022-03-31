291a58975373b96e3cb7ecb6457ff24b088eefef
omegafrog <jiwoo22110103@gmail.com>
2022-04-01 00:05:49 +0900

    java config파일을 통한 빈 생성
    context를 만듦으로서 java config파일에 있는 bean을 등록한다.

--- main/src/main/java/com/board/main/MainApplication.java
+++ main/src/main/java/com/board/main/MainApplication.java
@@ -1,10 +1,15 @@
 package com.board.main;
 
+import com.board.main.repository.BoardRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
+	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
