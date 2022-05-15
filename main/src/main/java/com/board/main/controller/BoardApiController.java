3b96046591dddd9d51c3a46caa57634e60ae46f5
omegafrog <jiwoo22110103@gmail.com>
2022-05-15 21:00:09 +0900

    lombok 생성자 annotation

--- main/src/main/java/com/board/main/controller/BoardApiController.java
+++ main/src/main/java/com/board/main/controller/BoardApiController.java
@@ -1,22 +1,13 @@
 package com.board.main.controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.board.main.BoardService;
+import com.board.main.domain.Board;
+import com.board.main.domain.Post;
+import com.board.main.domain.Posts;
 
-import com.board.main.repository.Board;
-import com.board.main.repository.BoardRepository;
-import com.board.main.repository.Post;
-import com.board.main.repository.Posts;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,9 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class BoardController {
-    @Autowired
-    private BoardRepository memoryBoardRepository;
+@RequiredArgsConstructor
+public class BoardApiController {
+
+    private final BoardService boardService;
 
     // 해당 id를 가진 보드의 모든 글을 보여줌.
     @GetMapping("/boards/{boardid}")
@@ -37,19 +29,22 @@ public class BoardController {
     public String getBoard(@PathVariable("boardid") int boardid, Model model) {
         // Board currentBoard = memoryBoardRepository.findById(boardid);
         // Posts currentPosts = memoryPostRepository.findById(postsid);
-        Board currentBoard = new Board();
-        currentBoard.setId(1);
-        currentBoard.setName("hi");
-        currentBoard.setPostsid(1);
-        Posts currentPosts = new Posts();
-        LinkedList contentList = new LinkedList<>();
-        contentList.add(new Post("no1", 1));
-        contentList.add(new Post("no2", 2));
-        contentList.add(new Post("no3", 3));
-        currentPosts.setContents(contentList);
+//        Board currentBoard = new Board();
+//        currentBoard.setId(1);
+//        currentBoard.setName("hi");
+//        currentBoard.setPostsid(1);
+//        Posts currentPosts = new Posts();
+//        LinkedList contentList = new LinkedList<>();
+//        contentList.add(new Post("no1", 1));
+//        contentList.add(new Post("no2", 2));
+//        contentList.add(new Post("no3", 3));
+//        currentPosts.setContents(contentList);
+
+
+
         // 대충 더미 데이터를 만들고 model에 board와 posts를 넣는다.
-        model.addAttribute("board", currentBoard);
-        model.addAttribute("posts", currentPosts);
+//        model.addAttribute("board", currentBoard);
+//        model.addAttribute("posts", currentPosts);
 
         return "boards/board";
     }
